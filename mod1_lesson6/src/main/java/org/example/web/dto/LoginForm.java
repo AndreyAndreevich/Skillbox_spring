903e8337db4b9db2b44d298dea7370e80869b8bf
Andrey A. Lukin <l.a.r.p@yandex.ru>
2020-12-14 22:33:42 +0300

    Added user registration

--- mod1_lesson6/src/main/java/org/example/web/dto/LoginForm.java
+++ mod1_lesson6/src/main/java/org/example/web/dto/LoginForm.java
@@ -1,8 +1,15 @@
 package org.example.web.dto;
 
+import java.util.Objects;
+
+import javax.validation.constraints.NotBlank;
+
 public class LoginForm {
 
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 
     public LoginForm(String username, String password) {
@@ -27,6 +34,24 @@ public class LoginForm {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LoginForm loginForm = (LoginForm) o;
+        return Objects.equals(username, loginForm.username) &&
+            Objects.equals(password, loginForm.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
     }
 
     @Override
