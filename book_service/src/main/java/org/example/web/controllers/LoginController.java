f3bcdc7091ab4c2ccae5bf50b56cb7c4b6b35221
Andrey A. Lukin <l.a.r.p@yandex.ru>
2020-12-24 15:38:15 +0300

    Added book login exception

--- book_service/src/main/java/org/example/web/controllers/LoginController.java
+++ book_service/src/main/java/org/example/web/controllers/LoginController.java
@@ -3,12 +3,14 @@ package org.example.web.controllers;
 import javax.validation.Valid;
 
 import org.apache.log4j.Logger;
+import org.example.app.exceptions.BookShelfLoginException;
 import org.example.app.services.LoginService;
 import org.example.web.dto.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +35,7 @@ public class LoginController {
     }
 
     @PostMapping("/auth")
-    public String authenticate(@Valid LoginForm loginForm, BindingResult result) {
+    public String authenticate(@Valid LoginForm loginForm, BindingResult result) throws BookShelfLoginException {
         if (result.hasErrors()) {
             logger.warn(result.getAllErrors());
             return "redirect:/login";
@@ -44,7 +46,7 @@ public class LoginController {
             return "redirect:/books/shelf";
         } else {
             logger.info("login FAIL redirect back to login");
-            return "redirect:/login";
+            throw new BookShelfLoginException("invalid username or password");
         }
     }
 
@@ -62,5 +64,11 @@ public class LoginController {
             logger.info("register FAIL redirect back to login");
             return "redirect:/login";
         }
+    }
+
+    @ExceptionHandler(BookShelfLoginException.class)
+    public String handleError(Model model, BookShelfLoginException exception) {
+        model.addAttribute("errorMessage", exception.getMessage());
+        return "errors/404";
     }
 }
