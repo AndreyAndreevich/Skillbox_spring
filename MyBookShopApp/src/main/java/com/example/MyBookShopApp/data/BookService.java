b8d64999274a458e9478820a48e8cb82ecd075cf
AndreyAndreevich <l.a.r.p@yandex.ru>
2021-01-28 00:07:07 +0300

    Added db

---
+++ MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -0,0 +1,35 @@
+package com.example.MyBookShopApp.data;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> getBooksData() {
+        List<Book> books = jdbcTemplate.query("SELECT * from books", (ResultSet rs, int rowNum) -> {
+            Book book = new Book();
+            book.setId(rs.getInt("id"));
+            book.setAuthor(rs.getString("author"));
+            book.setTitle(rs.getString("title"));
+            book.setPriceOld(rs.getString("priceOld"));
+            book.setPrice(rs.getString("price"));
+
+            return book;
+        });
+
+        return new ArrayList<>(books);
+    }
+}
