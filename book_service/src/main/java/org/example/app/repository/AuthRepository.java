3528d848072fac76eaa8f355a87164a15b57e344
AndreyAndreevich <l.a.r.p@yandex.ru>
2021-04-25 22:35:11 +0300

    Moved

--- book_service/src/main/java/org/example/app/repository/AuthRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.app.repository;
-
-public interface AuthRepository<T> {
-    boolean authenticate(T user);
-    boolean is_user_exist(T user);
-    void store(T user);
-}
--- book_service/src/main/java/org/example/web/dto/BookToRemove.java
+++
@@ -1,5 +0,0 @@
-package org.example.web.dto;
-
-public class BookToRemove extends Book {
-
-}
