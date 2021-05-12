b6a3a6817a4aa617f066e8bdca6b1972c8865fe1
AndreyAndreevich <l.a.r.p@yandex.ru>
2021-05-12 23:43:58 +0300

    Added migrations

---
+++ src/main/resources/db.changelog/01-initial.postgresql.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS authors
+(
+    id         BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS books
+(
+    id        BIGSERIAL PRIMARY KEY,
+    price     VARCHAR(250) DEFAULT NULL,
+    price_old VARCHAR(250) DEFAULT NULL,
+    title     VARCHAR(250) NOT NULL,
+    author_id BIGINT       NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS test_entities
+(
+    id   BIGSERIAL PRIMARY KEY,
+    data VARCHAR(250) NOT NULL
+)
