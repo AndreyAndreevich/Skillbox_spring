e83f9576749e4d048f9531ab984f230fc3bb930b
AndreyAndreevich <l.a.r.p@yandex.ru>
2021-05-16 20:00:07 +0300

    Updated author service

--- src/main/java/com/example/MyBookShopApp/service/AuthorService.java
+++ src/main/java/com/example/MyBookShopApp/service/AuthorService.java
@@ -1,34 +1,31 @@
 package com.example.MyBookShopApp.service;
 
-import com.example.MyBookShopApp.entity.Author;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
-
-import java.sql.ResultSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.example.MyBookShopApp.entity.Author;
+import com.example.MyBookShopApp.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class AuthorService {
 
-    private JdbcTemplate jdbcTemplate;
+    private final AuthorRepository authorRepository;
 
     @Autowired
-    public AuthorService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
     public Map<String, List<Author>> getAuthorsMap() {
-        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors",(ResultSet rs, int rowNum) -> {
-            Author author = new Author();
-            author.setId(rs.getInt("id"));
-            author.setFirstName(rs.getString("first_name"));
-            author.setLastName(rs.getString("last_name"));
-            return author;
-        });
-
-        return authors.stream().collect(Collectors.groupingBy((Author a) -> {return a.getLastName().substring(0,1);}));
+        return authorRepository.findAllBooks()
+            .stream()
+            .collect(
+                Collectors.groupingBy(
+                    author -> author.getLastName().substring(0, 1)
+                )
+            );
     }
 }
